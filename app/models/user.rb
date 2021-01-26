8ed822727115f9cff0ea40063ddec12b6594a18b
haku-noir <hakunoir.00@gmail.com>
2021-01-26 17:36:47 +0900

    add: Usersモデルにバリデーションの設定とパスワードのハッシュ化

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :password, allow_nil: true, length: { minimum: 6 }
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_072029) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
