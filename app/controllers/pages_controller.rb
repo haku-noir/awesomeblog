16273c47b2af8bb85603e187a45e463c50fb7cae
haku-noir <hakunoir.00@gmail.com>
2021-01-28 18:57:32 +0900

    update: ホームページで全ての投稿が表示されるように更新

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
     if session[:user_id]
+      @microposts = Micropost.all
       @micropost = Micropost.new
       render 'users/home_feed'
     end
