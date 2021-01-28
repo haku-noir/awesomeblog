846341a23126f180ba7818bad016872f93ef33a0
haku-noir <hakunoir.00@gmail.com>
2021-01-28 18:52:29 +0900

    add: 投稿機能を追加

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,7 +1,20 @@
 class MicropostsController < ApplicationController
   def create
+    @user = User.find(session[:user_id])
+    @micropost = @user.microposts.new(micropost_params)
+
+    if @micropost.save
+      redirect_to root_url
+    else
+      redirect_to 'users/home_feed'
+    end
   end
 
   def destroy
   end
+
+  private
+    def micropost_params
+      params.require(:micropost).permit(:content, :picture)
+    end
 end
