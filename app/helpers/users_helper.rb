f9b13b7049497e0b5b2a8fe5abe00c9142a7e163
haku-noir <hakunoir.00@gmail.com>
2021-01-28 17:32:44 +0900

    add: gravaterを用いたユーザ画像表示機能を追加

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    return "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  end
 end
